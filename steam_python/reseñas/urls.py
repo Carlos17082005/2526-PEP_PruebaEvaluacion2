64e190b85869c4b53d324aa74ce85e78ea244734
Álvaro Claudio de las Mozas <a.claudio0802@gmail.com>
2026-03-02 11:48:45 +0100

    Programo Vista DetallesJuego
    Ahora al pinchar en un juego muestra los detalles del juego

--- steam_python/reseñas/urls.py
+++ steam_python/reseñas/urls.py
@@ -3,10 +3,12 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from .views import VistaListaJuegos
+from .views import VistaListaJuegos, VistaDetalleJuego, VistaCrearJuego
 
 urlpatterns = [
     path("", VistaListaJuegos.as_view(), name="home"),
+    path("juego/<str:nombre_juego>/", VistaDetalleJuego.as_view(), name="detalle_juego"),
+    path("juego/nuevo/", VistaCrearJuego.as_view(), name="nuevo_juego"),
 ]
 
 if settings.DEBUG:
--- steam_python/reseñas/views.py
+++ steam_python/reseñas/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
-
-# Create your views here.
 from .models import Juego, Resena
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView, CreateView
 
 
 class VistaListaJuegos(ListView):
     model = Juego
     template_name = "home.html"
     context_object_name = "juegos"
+
+class VistaDetalleJuego(DetailView):
+    model = Juego
+    template_name = "detalle_juego.html"
+
+    # Le decimos a Django: "Busca en la columna 'nombre_juego' de la base de datos..."
+    slug_field = "nombre_juego" 
+    # "...el valor que te llega desde la URL llamado también 'nombre_juego'"
+    slug_url_kwarg = "nombre_juego"
+
+class VistaCrearJuego(CreateView):
+    model = Juego
+    template_name = "juego_post.html"
+    fields = ["nombre_juego", "imagen"]
