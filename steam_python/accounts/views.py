1b46531795004620a6d5148c96e0df3315f51831
Álvaro Claudio de las Mozas <a.claudio0802@gmail.com>
2026-03-02 09:54:16 +0100

    Nuevos Campos Registrarse
    Añado nuevos campos en el formulario automático que crea Django para registrarse. Añado camos 'first_name', 'last_name' y 'emali'

--- steam_python/accounts/views.py
+++ steam_python/accounts/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
-
-# Create your views here.
-from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+#para agregar campos "first_name", "last_name" y "email" al registrarse
+from .forms import CustomUserCreationForm 
 
 
 class SignUpView(CreateView):
-    form_class = UserCreationForm
+    form_class = CustomUserCreationForm 
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
+
+
