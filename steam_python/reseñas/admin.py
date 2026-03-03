f5dd1f6132d17b70b87422232b54dac80f6e91fd
Álvaro Claudio de las Mozas <a.claudio0802@gmail.com>
2026-03-03 14:07:10 +0100

    Ahora puedes ver los campos de la tablbla Juegos en la pantalla de Admin

--- steam_python/reseñas/admin.py
+++ steam_python/reseñas/admin.py
@@ -3,5 +3,9 @@ from django.contrib import admin
 # Register your models here.
 from .models import Resena, Juego
 
+class JuegoAdmin(admin.ModelAdmin): 
+    list_display = ("nombre_juego","autor","imagen")
+
+
 admin.site.register(Resena)
-admin.site.register(Juego)
+admin.site.register(Juego ,JuegoAdmin)
