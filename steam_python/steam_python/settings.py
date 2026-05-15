f1508d2248ae217ab6beb29444f11c59f0975036
Carlos Gabriel García Guzmán <carlosggarcia.guzman@gmail.com>
2026-05-15 12:45:46 +0200

    añade la libreria django-cleanup para evitar tener infinitas imagenes sin usar en el dir media/post
    django-cleanup detecta cambios (elimina  u juego o edita un juego) y en consecuencia elimina o no la imagen

--- steam_python/steam_python/settings.py
+++ steam_python/steam_python/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'reseñas', 'accounts'
+    'reseñas', 'accounts',
+    'django_cleanup.apps.CleanupConfig'
 ]
 
 MIDDLEWARE = [
